706ca801b36d381c818ec667c57ed7ff87fdd1b2
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-04-17 12:24:03 -0700

    this should work;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,26 @@
 let board_color1 = "#769656";
 let board_color2 = "#eeeed2";
 
+// math stuff
+let square_size = 64;
+x = 0;
+y = 0;
+
 // get canvas id and ctx 2d
 const elm = document.getElementById("board");
 const ctx = elm.getContext("2d");
 
 ctx.fillStyle = board_color2;
-ctx.fillRect(0, 0, 500, 500);
+ctx.fillRect(0, 0, 512, 512);
+
+// fill checkboard pattern
+ctx.fillStyle = board_color1;
+for (i = 0; i < 64; i++) {
+    x++
+    ctx.fillRect(x * square_size, y * square_size, (x + 1) * square_size, (x + 1) * square_size);
+
+    if (x > 8) {
+        x = 0;
+        y++;
+    }
+}
