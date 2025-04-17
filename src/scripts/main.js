2f2d5fee6e5d2f3810594c96fd1e3002163981bf
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-04-17 12:19:00 -0700

    hi

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
+// chessboard colors
+let board_color1 = "#769656";
+let board_color2 = "#eeeed2";
+
 // get canvas id and ctx 2d
 const elm = document.getElementById("board");
 const ctx = elm.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 600, 600);
+ctx.fillStyle = board_color2;
+ctx.fillRect(0, 0, 500, 500);
