a0cef89b57f0e1e04a0a0fe83076601776e5ca55
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-04-23 20:27:37 -0700

    radius 30

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -184,7 +184,7 @@ elm.addEventListener("mousedown", function (e) {
     ctx.fillStyle = "rgb(255, 0, 0, 0.5)";
 
     // ctx.fillRect(roundToSquareSize(square_size, getMousePos(elm, e).x), roundToSquareSize(square_size, getMousePos(elm, e).y), square_size, square_size);
-    drawCircle(roundToSquareSize(square_size, getMousePos(elm, e).x), roundToSquareSize(square_size, getMousePos(elm, e).y), "red", 70)
+    drawCircle(roundToSquareSize(square_size, getMousePos(elm, e).x), roundToSquareSize(square_size, getMousePos(elm, e).y), "red", 30)
   }
 
   // left click
